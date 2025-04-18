14b414b577341c3a2c485dccf3294419a38a89ce
Akshay448591 <akshayyadav12356@gmail.com>
2025-04-18 15:56:27 +0530

    react Projects

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Counter from './Projects/Counter/Counter'
+import ToDo from './Projects/ToDo/ToDo'
+import FormValidation from './Projects/FormValidation/FormValidation'
+const App = () => {
+  return (
+    <div>
+      {/* <Counter/> */}
+      {/* <ToDo/> */}
+      <FormValidation/>
+    </div>
+  )
+}
+
+export default App
